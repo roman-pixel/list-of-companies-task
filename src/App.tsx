8098601da21d29877593bd4f609f082addbf1ecf
Roman <romansrgvch@outlook.com>
2024-08-13 14:37:17 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
 import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
+
 import CompanyTable from './components/CompanyTable';
 import TableMenu from './components/TableMenu';
 import Header from './ui/Header';
 
+import { ThemeProvider } from './contexts/ThemeContext';
+import store from './store/store';
+
 function App() {
   return (
-    <>
-      <Header />
+    <Provider store={store}>
+      <ThemeProvider>
+        <Header />
 
-      <div className="sm:p-5">
-        <CompanyTable />
-        <TableMenu />
-      </div>
+        <div className="sm:p-5">
+          <CompanyTable />
+          <TableMenu />
+        </div>
 
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        gutter={10}
-        toastOptions={{
-          className:
-            'bg-white text-gray-800 dark:bg-slate-700 dark:text-gray-100',
-          success: {
-            duration: 3000,
-          },
-          error: {
-            duration: 5000,
-          },
-        }}
-      />
-    </>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          gutter={10}
+          toastOptions={{
+            className:
+              'bg-white text-gray-800 dark:bg-slate-700 dark:text-gray-100',
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+          }}
+        />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './store/store.ts';
-import { ThemeProvider } from './contexts/ThemeContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
 );
